f716f83c1afa4118e5fd54ad9bf6e47cbb1f6614
●┼WaSim Akram••✓⁩ <66058079+evildevill@users.noreply.github.com>
2020-11-01 10:32:44 +0500

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+apt-get update -y
+apt-get upgrade -y
+pkg install python2 -y
+pkg install python -y
+pip2 install lolcat
+pkg install wget -y
+pkg install git -y
+wget https://github.com/evildevill/Homo-Attack/raw/master/setup.sh
+cd Homo-Attack
+chmod +x *
+bash Homo-Attack.sh
